5b1ffee45050609413a15897107e94d34f40ecf8
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-02-17 14:39:29 +0100

    Minor changes
    - uint as unsigned int to not depend on stdint.h
    - key for vigner is now a TexBuffer

--- libhash/include/protocol.h
+++ libhash/include/protocol.h
@@ -3,6 +3,8 @@
 #ifndef PROTOCOL
 #define PROTOCOL
 
+typedef unsigned int uint;
+
 #define TXTLEN 256 // full message
 #define CNTLEN                                                                 \
   242 // full message - 4 hashcode - 1 space - 8 timestamp - 1 space
@@ -13,7 +15,7 @@ typedef struct {
   char content[CNTLEN];
 } Packet;
 
-typedef struct TextBuffer {
+typedef struct {
   char message[TXTLEN];
 } TextBuffer;
 
@@ -37,17 +39,17 @@ void RopeIt_next(RopeIterator *i);
 
 // Encryption
 enum Mode { ENCRYPT = 1, DECRYPT = -1 };
-void vigener(TextBuffer *msg, char *key, enum Mode mode);
+void vigener(TextBuffer *msg, const TextBuffer *key, enum Mode mode);
 
 // Hashing
 uint32_t djb2(Rope *str);
 uint16_t sysv(uint32_t somma);
 
 // Secrets
-const TextBuffer * getUnipdSig();
-const TextBuffer * getUnipdKey();
+const TextBuffer *getUnipdSig();
+const TextBuffer *getUnipdKey();
 
-const TextBuffer * getOtherSig();
-const TextBuffer * getOtherKey();
+const TextBuffer *getOtherSig();
+const TextBuffer *getOtherKey();
 
 #endif // PROTOCOL
