c9938deef7e0b0f002c12c265c6fd576a2110503
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-02-17 13:55:07 +0100

    All library functionalities moved to protocol.h

--- libhash/include/protocol.h
+++ libhash/include/protocol.h
@@ -1,20 +1,53 @@
+#include <stdint.h>
+
 #ifndef PROTOCOL
 #define PROTOCOL
 
-#define MSGLEN 256 // full message
-#define CNTLEN 242 // full message - 4 hashcode chars - 1 space - 8 timestamp chars - 1 space
+#define TXTLEN 256 // full message
+#define CNTLEN                                                                 \
+  242 // full message - 4 hashcode - 1 space - 8 timestamp - 1 space
 
-typedef struct Packet Packet;
-typedef struct Message Message;
-
-struct Packet {
-  unsigned short hashcode;
-  unsigned int timestamp;
+typedef struct {
+  uint16_t hashcode : 16;
+  uint32_t timestamp : 32;
   char content[CNTLEN];
+} Packet;
+
+typedef struct TextBuffer {
+  char message[TXTLEN];
+} TextBuffer;
+
+// Dynamic String allocation
+typedef struct Rope Rope;
+struct Rope {
+  TextBuffer *str;
+  Rope *next;
 };
 
-struct Message {
-  char message[MSGLEN];
-};
+typedef struct {
+  Rope *rope;
+  uint8_t idx : 8;
+} RopeIterator;
+
+Rope newRope(TextBuffer *str, Rope *next);
+RopeIterator RopeIt_begin(Rope *rope);
+char RopeIt_get(RopeIterator *i);
+int RopeIt_hasNext(RopeIterator *i);
+void RopeIt_next(RopeIterator *i);
+
+// Encryption
+enum Mode { ENCRYPT = 1, DECRYPT = -1 };
+void vigener(TextBuffer *msg, char *key, enum Mode mode);
+
+// Hashing
+uint32_t djb2(Rope *str);
+uint16_t sysv(uint32_t somma);
+
+// Secrets
+const TextBuffer * getUnipdSig();
+const TextBuffer * getUnipdKey();
+
+const TextBuffer * getOtherSig();
+const TextBuffer * getOtherKey();
 
 #endif // PROTOCOL
