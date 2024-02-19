cbf8966002d9d6bc406ad27c5805b49d147ed0ec
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-02-19 14:47:05 +0100

    Updated sources/main.c

--- libhash/sources/main.c
+++ libhash/sources/main.c
@@ -12,26 +12,13 @@ void print_rope(Rope *rope) {
   }
 }
 
-void print_textbuffer(TextBuffer *txt) {
-  printf("TextBuffer{ '%s' }", txt->message);
-}
+void print_textbuffer(TextBuffer *txt) { printf("TextBuffer{ '%s' }", txt->message); }
 
 void print_packet(Packet *pkg) {
   printf("Packet{ %8x, %8x,", pkg->hashcode, pkg->timestamp);
   print_textbuffer(&pkg->content);
   printf(" }");
 }
-
-Packet pkgs[8] = {
-    {.hashcode = 0x60d9, .timestamp = 0, .content = {.message = "andakjsnfkedjnfskdgjnsdfks"}},
-    {.hashcode = 0x6940, .timestamp = 0, .content = {.message = "AAAAAAAAAAAAAAAAAAAAaa"}},
-    {.hashcode = 0xff08, .timestamp = 0, .content = {.message = "eeeeeeeeee"}},
-    {.hashcode = 0x879a, .timestamp = 0, .content = {.message = "eeeeeeeeef"}},
-    {.hashcode = 0xcce1, .timestamp = 0, .content = {.message = "eeefeeeeee"}},
-    {.hashcode = 0x4450, .timestamp = 0, .content = {.message = "eeefeeeeed"}},
-    {.hashcode = 0xb9c0, .timestamp = 0, .content = {.message = "eeedeeeeef"}},
-    {.hashcode = 0xd8e1, .timestamp = 0, .content = {.message = "a b c d e"}},
-};
 
 typedef union {
   struct {
@@ -53,7 +40,7 @@ unsigned int djb2_real(char *str) {
   return hash;
 }
 
-unsigned short sysv_real(unsigned int sum){
+unsigned short sysv_real(unsigned int sum) {
   const int a = 65536;
   const long b = 4294967295;
   unsigned int r = (sum % a) + ((sum % b) / a);
@@ -61,6 +48,17 @@ unsigned short sysv_real(unsigned int sum){
 }
 
 int main(int argc, char **argv) {
+  Packet pkgs[8] = {
+      {.hashcode = 0x60d9, .timestamp = 0, .content = {.message = "andakjsnfkedjnfskdgjnsdfks"}},
+      {.hashcode = 0x6940, .timestamp = 0, .content = {.message = "AAAAAAAAAAAAAAAAAAAAaa"}},
+      {.hashcode = 0xff08, .timestamp = 0, .content = {.message = "eeeeeeeeee"}},
+      {.hashcode = 0x879a, .timestamp = 0, .content = {.message = "eeeeeeeeef"}},
+      {.hashcode = 0xcce1, .timestamp = 0, .content = {.message = "eeefeeeeee"}},
+      {.hashcode = 0x4450, .timestamp = 0, .content = {.message = "eeefeeeeed"}},
+      {.hashcode = 0xb9c0, .timestamp = 0, .content = {.message = "eeedeeeeef"}},
+      {.hashcode = 0xd8e1, .timestamp = 0, .content = {.message = "a b c d e"}},
+  };
+
   for (int i = 0; i < 8; i++) {
     Packet *p = &(pkgs[i]);
     char str[512];
@@ -73,20 +71,20 @@ int main(int argc, char **argv) {
     SysvNumber h = djb2(&s);
 
     printf("DJB2\n");
-    printf("CStr: %x '%s'\n",djb2_real(str),str);
-    printf("Rope: %x '",h.u32b);
+    printf("CStr: %x '%s'\n", djb2_real(str), str);
+    printf("Rope: %x '", h.u32b);
     print_rope(&s);
     printf("'\n");
 
     printf("SYSV\n");
-    printf("Real: %x -> %x\n",djb2_real(str), sysv_real(djb2_real(str)));
-    printf("Unio: %x -> %x\n",h.u32b,sysv(h).u16bl);
+    printf("Real: %x -> %x\n", djb2_real(str), sysv_real(djb2_real(str)));
+    printf("Unio: %x -> %x\n", h.u32b, sysv(h).u16bl);
 
     printf("VALIDATION\n");
-    printf("unipd sig     : %d\n",validate(p, getUnipdSig()));
-    printf("3rd party sig : %d\n",validate(p, getOtherSig()));
+    printf("unipd sig     : %d\n", validate(p, getUnipdSig()));
+    printf("3rd party sig : %d\n", validate(p, getOtherSig()));
 
-    // TODO : add timestamp to 
+    // TODO : add timestamp to
     printf("HASHING\n");
     printf("BEFORE : ");
     print_packet(p);
