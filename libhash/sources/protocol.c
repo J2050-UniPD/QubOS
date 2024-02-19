aa10bbf0a688375ba75d60ebd934401dae8e9c69
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-02-19 19:24:02 +0100

    Parsing returns true if success, false otherwise

--- libhash/sources/protocol.c
+++ libhash/sources/protocol.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 int parsePacketToText(Packet *packet, TextBuffer *msg) {
-  return sprintf(msg->message, "%4x %08x %s", packet->hashcode, packet->timestamp, packet->content.message);
+  return sprintf(msg->message, "%04x %08x %s", packet->hashcode, packet->timestamp, packet->content.message) > 0;
 }
 
 int parseTextToPacket(TextBuffer *msg, Packet *packet) {
   static uint hashcode, timestamp, errcode;
-  errcode = sscanf(msg->message, "%4x %8x %242[a-zA-Z0-9 ]s", &hashcode, &timestamp, packet->content.message);
+  errcode = sscanf(msg->message, "%04x %8x %242[a-zA-Z0-9 ]s", &hashcode, &timestamp, packet->content.message);
   packet->hashcode = hashcode;
   packet->timestamp = timestamp;
-  return errcode;
+  return errcode > 0;
 }
