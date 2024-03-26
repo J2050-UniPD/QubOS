aa3a82a215cd0ee9399a2fb1db5b8f3a83aa4b03
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-03-26 09:21:48 +0100

    TextBuffer -> ByteBuffer

--- libhash/include/protocol.h
+++ libhash/include/protocol.h
@@ -3,26 +3,28 @@
 
 typedef unsigned int uint;
 
-#define TXTLEN 256 // Max size of a message sent from ground
+// byte is strictly 8 bits
+typedef struct {
+  uint value : 8;
+} byte;
+
+// Max size of a message sent from ground
+#define TXTLEN 256
 
 typedef struct {
-  char message[TXTLEN];
-} TextBuffer;
+  byte buffer[TXTLEN];
+} ByteBuffer;
 
 typedef struct {
-  uint hashcode : 16;
+  uint hashcode : 32;
   uint timestamp : 32;
-  TextBuffer content;
+  ByteBuffer * load;
 } Packet;
-
-// parsing
-int parsePacketToText(Packet * packet, TextBuffer * msg);
-int parseTextToPacket(TextBuffer * msg, Packet * packet);
 
 // Dynamic String allocation
 typedef struct Rope Rope;
 struct Rope {
-  const TextBuffer *str;
+  const ByteBuffer *str;
   Rope *next;
 };
 
@@ -32,23 +34,23 @@ typedef struct {
 } RopeIterator;
 
 RopeIterator RopeIterator_begin(Rope *rope);
-char         RopeIterator_get(RopeIterator *i);
-int          RopeIterator_hasNext(RopeIterator *i);
-int          RopeIterator_next(RopeIterator *i);
+char RopeIterator_get(RopeIterator *i);
+int RopeIterator_hasNext(RopeIterator *i);
+int RopeIterator_next(RopeIterator *i);
 
 // Encryption
 enum Mode { ENCRYPT = 1, DECRYPT = -1 };
-void vigener(TextBuffer *msg, const TextBuffer *key, enum Mode mode);
+void vigener(ByteBuffer *msg, const ByteBuffer *key, enum Mode mode);
 
 // Hashing
-void hash(Packet *, const TextBuffer *);
-int validate(const Packet *, const TextBuffer *);
+void hash(Packet *, const ByteBuffer *);
+int validate(const Packet *, const ByteBuffer *);
 
 // Secrets
-const TextBuffer *getUnipdSig();
-const TextBuffer *getUnipdKey();
+const ByteBuffer *getUnipdSig();
+const ByteBuffer *getUnipdKey();
 
-const TextBuffer *getOtherSig();
-const TextBuffer *getOtherKey();
+const ByteBuffer *getOtherSig();
+const ByteBuffer *getOtherKey();
 
 #endif // PROTOCOL
