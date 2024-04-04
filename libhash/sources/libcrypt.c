ba046e2ae93b91fd66d6dd1ab5e164fe03bdb2e2
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-04-04 21:59:02 +0200

    sizeof buffer instead of macro

--- libhash/sources/libcrypt.c
+++ libhash/sources/libcrypt.c
@@ -1,7 +1,7 @@
 #include <protocol.h>
 
 void vigener(Message *msg, const Message *key, enum Mode mode) {
-  for (u32 i = 0; i < TXTLEN; i++) {
+  for (u32 i = 0; i < sizeof(msg->buffer); i++) {
     msg->buffer[i] = msg->buffer[i] + mode * key->buffer[i];
   }
 }
