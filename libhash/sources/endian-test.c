ebacd04f8667ab1279b312431c3d711922ebe364
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-04-04 18:59:44 +0200

    updated endian-test

--- libhash/sources/endian-test.c
+++ libhash/sources/endian-test.c
@@ -1,44 +1,44 @@
 #include <protocol.h>
 
-uint assert(Message * m){
-  int load = 0;
-  for(; load + 8 < TXTLEN; load += (m->buffer[8+load].value == m->load[load].value));
-  return load
-  + ((((m->hashcode      >> (0)*8) & 0xff) == m->buffer[0].value))
-  + ((((m->hashcode      >> (1)*8) & 0xff) == m->buffer[1].value))
-  + ((((m->hashcode      >> (2)*8) & 0xff) == m->buffer[2].value))
-  + ((((m->hashcode      >> (3)*8) & 0xff) == m->buffer[3].value))
-  + ((((m->timestamp     >> (0)*8) & 0xff) == m->buffer[4].value))
-  + ((((m->timestamp     >> (1)*8) & 0xff) == m->buffer[5].value))
-  + ((((m->timestamp     >> (2)*8) & 0xff) == m->buffer[6].value))
-  + ((((m->timestamp     >> (3)*8) & 0xff) == m->buffer[7].value))
+u32 assert(Message * m){
+  u32 assertion = 0;
+  for(u32 i = 0; i < sizeof(m->load); i++, assertion++);
+  return assertion
+  + ((((m->hash >> (0)*8) & 0xff) == m->buffer[0]))
+  + ((((m->hash >> (1)*8) & 0xff) == m->buffer[1]))
+  + ((((m->hash >> (2)*8) & 0xff) == m->buffer[2]))
+  + ((((m->hash >> (3)*8) & 0xff) == m->buffer[3]))
+  + ((((m->time >> (0)*8) & 0xff) == m->buffer[4]))
+  + ((((m->time >> (1)*8) & 0xff) == m->buffer[5]))
+  + ((((m->time >> (2)*8) & 0xff) == m->buffer[6]))
+  + ((((m->time >> (3)*8) & 0xff) == m->buffer[7]))
   ;
 }
 
 void test_endianness(void) {
   Message m = {};
-  m.hashcode = 0x87654321;
-  m.timestamp = 0x01020304;
-  for(int i = 0; i + 8 < TXTLEN; m.load[i++].value = 0);
+  m.hash = 0x87654321;
+  m.time = 0x01020304;
+  for(u32 i = 0; i < sizeof(m.load); m.load[i++] = 0);
 
-  uint assertGen, assertHash, assertTime, assertBuff;
+  u32 assertGen, assertHash, assertTime, assertBuff;
   assertGen  = assert(&m);
 
-  m.hashcode = 0x1312141;
+  m.hash = 0x1312141;
   assertHash = assert(&m); 
 
-  m.timestamp = 0xdeadbeef;
+  m.time = 0xdeadbeef;
   assertTime = assert(&m);
 
-  m.buffer[0].value = 7;
-  m.buffer[1].value = 2;
-  m.buffer[2].value = 3;
-  m.buffer[6].value = 1;
-  m.buffer[7].value = 0;
-  m.buffer[16+1].value = 2+37;
-  m.buffer[16+2].value = 3+37;
-  m.buffer[16+6].value = 1+37;
-  m.buffer[16+7].value = 0+37;
+  m.buffer[0] = 7;
+  m.buffer[1] = 2;
+  m.buffer[2] = 3;
+  m.buffer[6] = 1;
+  m.buffer[7] = 0;
+  m.buffer[16+1] = 2+37;
+  m.buffer[16+2] = 3+37;
+  m.buffer[16+6] = 1+37;
+  m.buffer[16+7] = 0+37;
   assertBuff = assert(&m);
 
   if(
