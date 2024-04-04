b800537b89b80eae880bacd23fd4eb04cbba7675
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-04-04 17:46:17 +0200

    usage of pure bytes instead of text

--- libhash/sources/libcrypt.c
+++ libhash/sources/libcrypt.c
@@ -1,32 +1,7 @@
 #include <protocol.h>
-#include <stdio.h>
 
-size_t mod(int num, size_t div) {
-  return num < 0 ? ((-num * div + num) % div) : (num % div);
-}
-
-size_t indexOf(char c) {
-  if ('a' <= c && c <= 'z') {
-    return (c - 'a');
-  } else if ('A' <= c && c <= 'Z') {
-    return 26 + (c - 'A');
-  } else if ('0' <= c && c <= '9') {
-    return 26 + 26 + (c - '0');
-  } else {
-    return 26 + 26 + 10;
-  }
-}
-
-#define ALPHALEN 63
-
-// Preconditions:
-// - The key uses all 256 bytes allowed by the TextBuffer
-void vigener(TextBuffer *msg, const TextBuffer *key, enum Mode mode) {
-  static const char alphabet[ALPHALEN] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
-  size_t idx, offset;
-  for (size_t i = 0; i < TXTLEN && msg->message[i] != '\0'; i++) {
-    idx = indexOf(msg->message[i]);
-    offset = mode * indexOf(key->message[i]);
-    msg->message[i] = alphabet[mod(idx + offset, ALPHALEN)];
+void vigener(Message *msg, const Message *key, enum Mode mode) {
+  for (u32 i = 0; i < TXTLEN; i++) {
+    msg->buffer[i] = msg->buffer[i] + mode * key->buffer[i];
   }
 }
