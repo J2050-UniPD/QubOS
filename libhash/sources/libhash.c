08b804df94ce93645f1005427951b0b03311a16c
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-04-04 23:28:44 +0200

    defined validate

--- libhash/sources/libhash.c
+++ libhash/sources/libhash.c
@@ -10,3 +10,7 @@ u32 djb2(u32 digest, const Message *m) {
 void hash(Message *pkg, const Message *sig) {
   pkg->hash = djb2(djb2(5381, sig), pkg);
 }
+
+u32 validate(const Message *pkg, const Message *sig){
+  return pkg->hash == djb2(djb2(5381, sig), pkg);
+}
